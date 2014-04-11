3f4da89b2435707181018e5dec1674eb347ce60b
photon <photonxp@gmail.com>
2014-04-11 14:50:58 +0800

    Added class CommonMachine to hold common methods for various slice machines.

--- operations.py
+++ operations.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python
 
+
 """
 to do:
 *Push into git
-*Add parent class SliceMachine for various SliceMachines. So some code can reused.
-""""
+*Add parent class SliceMachine for various SliceMachines. So some code can be reused.
+"""
+
+'''
+Added CommonMachine Class to hold common methods
+Move _begin_partial in child classes to CommonMachine 
+'''
+
 
 """Define operations for different indications on letters"""
 
-class SliceMachine1():
-    """slice when no need to handle existed word partial"""
-    def __init__(self, readed_sequence=None):
-        if readed_sequence is not None:
-            self.readed_sequence = readed_sequence
-
+class CommonMachine():
     def _begin_partial(self, readed_sequence=None):
         if readed_sequence is not None:
             self.new_partial = readed_sequence[0]
         else:
             self.new_partial = self.readed_sequence[0]
 
+class SliceMachine1(CommonMachine):
+    """slice when no need to handle existed word partial"""
+    def __init__(self, readed_sequence=None):
+        if readed_sequence is not None:
+            self.readed_sequence = readed_sequence
+
     def slice_only_partial(self):
         self._begin_partial()
         self.slice_result = [self.new_partial]
         return self.slice_result
 
-class SliceMachine2():
+class SliceMachine2(CommonMachine):
     """slice when need to handle existed word partial"""
     def __init__(self, partial=None, readed_sequence=None):
         if partial is not None:
@@ -34,12 +42,11 @@ class SliceMachine2():
             self.readed_sequence = readed_sequence
             print "self.readed_sequence:", self.readed_sequence
 
-    def _begin_partial(self, readed_sequence=None):
-        if readed_sequence is not None:
-            self.new_partial =  readed_sequence[0]
-        else:
-
-            self.new_partial = self.readed_sequence[0]
+#    def _begin_partial(self, readed_sequence=None):
+#        if readed_sequence is not None:
+#            self.new_partial =  readed_sequence[0]
+#        else:
+#            self.new_partial = self.readed_sequence[0]
 
     def _cut_previous(self):
         self.cutted = self.previous_partial
